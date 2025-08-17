15fc6a6968d70f80e04fd3325a5246b29ebbf685
mhgholamrezaei <gholamrezaei.mh@gmail.com>
2025-08-17 11:32:23 -0400

    Implement SEL operation for bit-serial PIM architectures

--- tests/test-generic-AAP/test-aap.cpp
+++ tests/test-generic-AAP/test-aap.cpp
@@ -80,7 +80,7 @@ public:
 protected:
   std::string m_name;
   unsigned m_numSubarrayPerBank = 8;
-  unsigned m_numRows = 128;
+  unsigned m_numRows = 256;
   unsigned m_numCols = 256;
   unsigned m_numElements = 8;
   const std::vector<int> m_src1   = {0, 0, 0, 0, 1, 1, 1, 1};
@@ -102,6 +102,57 @@ public:
   TestComputeDRAM() : TestPim("ComputeDRAM") {}
   virtual ~TestComputeDRAM() {}
   virtual void runCore() {
+    PimObjId objSelNot = pimAllocAssociated(m_objSel, PIM_INT32); assert(objSelNot != -1);
+    PimObjId obj1Not = pimAllocAssociated(m_obj1, PIM_INT32); assert(obj1Not != -1);
+    PimObjId obj2Not = pimAllocAssociated(m_obj2, PIM_INT32); assert(obj2Not != -1);
+    PimObjId objDestNot = pimAllocAssociated(m_objDest, PIM_INT32); assert(objDestNot != -1);
+    pimNot(m_objSel, objSelNot);
+    pimNot(m_obj1, obj1Not);
+    pimNot(m_obj2, obj2Not);
+
+    // t0 = ROW_CLONE RS1        // seed RS1
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_obj1, 0}}, {{m_objTmp, 0}});
+    // t1 = ROW_CLONE SEL        // seed SEL
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objSel, 0}}, {{m_objTmp, 1}});
+    // t1 = AND t1, t0           // A = SEL & RS1      (t1,t0 overwritten→A)
+    pimGenericAAP(PimAnalogOpEnum::AND2, {{m_objTmp, 1}, {m_objTmp, 0}});
+    // t2 = ROW_CLONE t1        // spill A
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objTmp, 1}}, {{m_objTmp, 2}});
+    // t0 = ROW_CLONE RS2        // seed RS2
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_obj2, 0}}, {{m_objTmp, 0}});
+    // t1 = ROW_CLONE ~SEL       // seed ~SEL 
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{objSelNot, 0}}, {{m_objTmp, 1}});
+    // t1 = AND t1, t0           // B = ~SEL & RS2      (t1,t0 overwritten→B)
+    pimGenericAAP(PimAnalogOpEnum::AND2, {{m_objTmp, 1}, {m_objTmp, 0}});
+    // t0 = ROW_CLONE t2        //  load A
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objTmp, 2}}, {{m_objTmp, 0}});
+    // t0 = OR  t0, t1          // RD = A | B
+    pimGenericAAP(PimAnalogOpEnum::OR2, {{m_objTmp, 0}, {m_objTmp, 1}});
+    // RD = ROW_CLONE t0       //  store result
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objTmp, 0}}, {{m_objDest, 0}});
+    
+
+    // t0 = ROW_CLONE ~RS1        // seed ~RS1
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{obj1Not, 0}}, {{m_objTmp, 0}});
+    // t1 = ROW_CLONE SEL        // seed SEL
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objSel, 0}}, {{m_objTmp, 1}});
+    // t1 = AND t1, t0           // A = SEL & ~RS1      (t1,t0 overwritten→A)
+    pimGenericAAP(PimAnalogOpEnum::AND2, {{m_objTmp, 1}, {m_objTmp, 0}});
+    // t2 = ROW_CLONE t1        // spill A
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objTmp, 1}}, {{m_objTmp, 2}});
+    // t0 = ROW_CLONE ~RS2        // seed ~RS2
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{obj2Not, 0}}, {{m_objTmp, 0}});
+    // t1 = ROW_CLONE ~SEL       // seed ~SEL 
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{objSelNot, 0}}, {{m_objTmp, 1}});
+    // t1 = AND t1, t0           // B = ~SEL & ~RS2      (t1,t0 overwritten→B)
+    pimGenericAAP(PimAnalogOpEnum::AND2, {{m_objTmp, 1}, {m_objTmp, 0}});
+    // t0 = ROW_CLONE t2        //  load A
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objTmp, 2}}, {{m_objTmp, 0}});
+    // t0 = OR  t0, t1          // RD = A | B
+    pimGenericAAP(PimAnalogOpEnum::OR2, {{m_objTmp, 0}, {m_objTmp, 1}});
+    // ~RD = ROW_CLONE t0       //  store result
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objTmp, 0}}, {{objDestNot, 0}});
+
   }
 private:
   PimObjId m_negObj1 = -1;
@@ -112,6 +163,27 @@ public:
   TestSIMDRAM() : TestPim("SIMDRAM") {}
   virtual ~TestSIMDRAM() {}
   virtual void runCore() {
+    PimObjId objSelNot = pimCreateDualContactRef(m_objSel);
+    // t1 = AAP(RS2)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_obj2, 0}}, {{m_objTmp, 1}});
+    // t2 = AAP(ZERO)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objZero, 0}}, {{m_objTmp, 2}});
+    // t3 = AAP(~SEL)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{objSelNot, 0}}, {{m_objTmp, 3}});
+    // t4 = AAP(t1, t2, t3, MAJ3) // AND
+    pimGenericAAP(PimAnalogOpEnum::MAJ3, {{m_objTmp, 1}, {m_objTmp, 2}, {m_objTmp, 3}}, {{m_objTmp, 4}});
+    // t1 = AAP(SEL)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objSel, 0}}, {{m_objTmp, 1}});
+    // t2 = AAP(RS1)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_obj1, 0}}, {{m_objTmp, 2}});
+    // t3 = AAP(ZERO)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objZero, 0}}, {{m_objTmp, 3}});
+    // t5 = AAP(t1, t2, t3, MAJ3) // AND 
+    pimGenericAAP(PimAnalogOpEnum::MAJ3, {{m_objTmp, 1}, {m_objTmp, 2}, {m_objTmp, 3}}, {{m_objTmp, 5}});
+    // t2 = AAP(ONE)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objOne, 0}}, {{m_objTmp, 2}});
+    // RD = AAP(t5, t2, t4, MAJ3) // OR 
+    pimGenericAAP(PimAnalogOpEnum::MAJ3, {{m_objTmp, 5}, {m_objTmp, 2}, {m_objTmp, 4}}, {{m_objDest, 0}});
   }
 };
 
@@ -129,11 +201,41 @@ public:
   }
 };
 
+class TestFlexiDRAM: public TestPim {
+public:
+  TestFlexiDRAM() : TestPim("FlexiDRAM") {}
+  virtual ~TestFlexiDRAM() {}
+  virtual void runCore() {
+    // t1 = ROW_CLONE(RS1)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_obj1, 0}}, {{m_objTmp, 1}});
+    // t2 = ROW_CLONE(RS2)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_obj2, 0}}, {{m_objTmp, 2}});
+    // t4 = ROW_CLONE(ZERO)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objZero, 0}}, {{m_objTmp, 4}});
+    // t3 = XOR3(t1, t2, t4) // t3 = RS1 ⊕ RS2
+    pimGenericAAP(PimAnalogOpEnum::XOR3, {{m_objTmp, 1}, {m_objTmp, 2}, {m_objTmp, 4}}, {{m_objTmp, 3}});
+    // t0 = ROW_CLONE(SEL)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objSel, 0}}, {{m_objTmp, 0}});
+    // t4 = ROW_CLONE(ZERO)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objZero, 0}}, {{m_objTmp, 4}});
+    // t1 = MAJ3(t0, t3, t4) // t1 = SEL & (RS1 ⊕ RS2)
+    pimGenericAAP(PimAnalogOpEnum::MAJ3, {{m_objTmp, 0}, {m_objTmp, 3}, {m_objTmp, 4}}, {{m_objTmp, 1}});
+    // t2 = ROW_CLONE(RS2)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_obj2, 0}}, {{m_objTmp, 2}});
+    // t4 = ROW_CLONE(ZERO)
+    pimGenericAAP(PimAnalogOpEnum::ROW_CLONE, {{m_objZero, 0}}, {{m_objTmp, 4}});
+    // RD = XOR3(t2, t1, t4) // RD = RS2 ⊕ (SEL & (RS1 ⊕ RS2))
+    pimGenericAAP(PimAnalogOpEnum::XOR3, {{m_objTmp, 2}, {m_objTmp, 1}, {m_objTmp, 4}}, {{m_objDest, 0}});
+  }
+};
+
+
 class TestDRISA1T1CNor : public TestPim {
 public:
   TestDRISA1T1CNor() : TestPim("DRISA-1T1C-nor") {}
   virtual ~TestDRISA1T1CNor() {}
   virtual void runCore() {
+    
   }
 };
 
@@ -150,16 +252,28 @@ int main()
   std::cout << "PIM test: Generic AAP" << std::endl;
   // Add scope for ctor/dtor sequence
   {
-    TestComputeDRAM test1;
-    test1.run();
+    TestComputeDRAM testComputeDRAM;
+    testComputeDRAM.run();
   }
   {
-    TestSIMDRAM test2;
-    test2.run();
+    TestSIMDRAM testSIMDRAM;
+    testSIMDRAM.run();
   }
   {
-    TestReDRAM test3;
-    test3.run();
+    TestReDRAM testReDRAM;
+    testReDRAM.run();
+  }
+  {
+    TestFlexiDRAM testFlexiDRAM;
+    testFlexiDRAM.run();
+  }
+  {
+    TestDRISA1T1CNor testDRISA1T1CNor;
+    testDRISA1T1CNor.run();
+  }
+  {
+    TestDRISA1T1CMixed testDRISA1T1CMixed;
+    testDRISA1T1CMixed.run();
   }
   return 0;
 }
